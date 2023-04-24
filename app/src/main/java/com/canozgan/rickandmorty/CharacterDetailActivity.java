afaa2ea0067cd36599d7538b6433d0fa60c418c0
Can Ozgan <canozgan123@gmail.com>
2023-04-24 20:19:12 +0300

    Character detail page created

--- app/src/main/java/com/canozgan/rickandmorty/CharacterDetailActivity.java
+++ app/src/main/java/com/canozgan/rickandmorty/CharacterDetailActivity.java
@@ -4,11 +4,46 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CharacterDetailActivity extends AppCompatActivity {
+import com.canozgan.rickandmorty.databinding.ActivityCharacterDetailBinding;
+import com.squareup.picasso.Picasso;
 
+public class CharacterDetailActivity extends AppCompatActivity {
+    ActivityCharacterDetailBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_character_detail);
+        binding=ActivityCharacterDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        Singleton singleton=Singleton.getInstance();
+        CharacterModel selectedCharacterModel= singleton.selectedCharacterModel;
+        binding.titleTextView.setText(selectedCharacterModel.name);
+        binding.statusTextView.setText(selectedCharacterModel.status);
+        binding.specyTextView.setText(selectedCharacterModel.specy);
+        binding.genderTextView.setText(selectedCharacterModel.gender);
+        binding.originTextView.setText(selectedCharacterModel.origin.originName);
+        binding.locationTextView.setText(selectedCharacterModel.location.locationName);
+        binding.createdAtTextView.setText(selectedCharacterModel.createdAt);
+        if(selectedCharacterModel.episodeUrls.size()!=0){
+            String BASE_URL_FOR_EPISODES="https://rickandmortyapi.com/api/episode/";
+            String Episodes ="";
+            for(String episodeUrl : singleton.selectedCharacterModel.episodeUrls){
+                String episodeId ="";
+                for(int i=BASE_URL_FOR_EPISODES.length();i<episodeUrl.length();i++){
+                    episodeId= episodeId+ episodeUrl.charAt(i);
+                }
+                episodeId=episodeId+", ";
+                Episodes=Episodes+episodeId;
+            }
+            Episodes=Episodes.substring(0,Episodes.length()-2);
+            binding.episodesTextView.setText(Episodes);
+        }else{
+            binding.episodesTextView.setText("no episode");
+        }
+        try {
+            Picasso.get().load(selectedCharacterModel.imageUrl).into(binding.imageView2);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
     }
 }
